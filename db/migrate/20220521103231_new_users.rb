4b2ed139841230f639cd847f52e29a0a5ca6fc90
d3 <dsmart2080@gmail.com>
2022-05-21 15:46:51 -0600

    Added additional fields to Users Table

--- db/migrate/20220521103231_new_users.rb
+++ db/migrate/20220521103231_new_users.rb
@@ -3,6 +3,10 @@ class NewUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :gender
+      t.string :birthday
     end
 
   end
