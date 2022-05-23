48acdc814930da559ad76192bb960cd1abf6d5e9
dswe2022 <dswe2022@gmail.com>
2022-05-22 22:04:04 -0600

    Added Friendships Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_032719) do
+ActiveRecord::Schema.define(version: 2022_05_23_040303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2022_05_23_032719) do
     t.integer "author_id"
     t.integer "post_id"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
