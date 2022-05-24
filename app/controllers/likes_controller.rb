464cb482075d0e3e114f875ea4bf5265ede179b6
d3 <dsmart2080@gmail.com>
2022-05-23 22:31:39 -0600

    create Likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,27 @@
+class Api::LikesController < ApplicationController
+
+    def create 
+        like = Like.new(like_params)
+
+        if like.save
+            render json: like, status: :created
+        else
+            render json: {error: 'Like unsuccessful'}, status: :unprocessable_entity
+        end
+    end
+
+
+    def destroy
+        like = Like.find_by(id: params[:id])
+        like.destroy
+        render json: like
+    end
+
+    private
+
+    def like_params
+        params.permit(:liker_id, :post_id)
+    end
+
+
+end
