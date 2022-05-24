0255dcecae3cdb9acd49ecbccf8e6623a26a02b5
d3 <dsmart2080@gmail.com>
2022-05-23 22:44:23 -0600

    created friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,28 @@
 class Api::FriendshipsController < ApplicationController
 
     def create
-        friendship =
+        friendship = Friendship.new(friendship_params)
+
+        if friendship.save
+            render json: friendship, status: :created
+        else
+
+            render json: {error: 'Friendship unsuccessful'}, status: :unprocessable_entity
+        end
     end
+
+
+    def destroy
+        friendship = Friendship.find_by(id: params[:id])
+        friendship.destroy
+        render json: friendship
+    end
+
+
+    private
+
+    def friendship_params
+        params.permit(:user_id, :friend_id)
+    end
+
 end
