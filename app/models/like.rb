a22bc3f233591196e365e9849e7380b4eb4acbd2
d3 <dsmart2080@gmail.com>
2022-05-23 18:23:06 -0600

    created like model

---
+++ app/models/like.rb
@@ -0,0 +1,13 @@
+class Like < ApplicationRecord
+
+    belongs_to :liker,
+        primary_key: :id,
+        foreign_key: :liker_id,
+        class_name: :User
+    
+    belongs_to :post,
+        primary_key: :id,
+        foreign_key: :post_id,
+        class_name: :Post
+
+end
