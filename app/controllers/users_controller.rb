e71b5df35dc2f7b89f576ce41385aa9104871bd5
dswe2022 <dswe2022@gmail.com>
2022-05-23 18:46:41 -0600

    added create function to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,15 @@ class Api::UsersController < ApplicationController
         render json: users, status: :ok
     end
 
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] ||= user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
 
-    
 
 end
