841f7686c39d0c01a157d94b6a1e85db5ef64362
d3 <dsmart2080@gmail.com>
2022-05-23 18:01:07 -0600

    created post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,15 +6,25 @@ class Post < ApplicationRecord
     
     has_many :comments,
         primary_key: :id,
-        forieng_key: :post_id,
+        foreign_key: :post_id,
         class_name: :Comment
     
     has_many :likes,
         primary_key: :id,
         foreign_key: :post_id,
-        class_name: :Comment
+        class_name: :Like
 
-        
+    has_many :likers,
+        through: :likes,
+        source: :liker
+
+    belongs_to :wall,
+        primary_key: :id,
+        foreign_key: :recipient_id,
+        class_name: :User
+
+    has_one_attached :post_photo
+
 
 
 end
