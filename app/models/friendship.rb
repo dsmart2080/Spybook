2926dbc05e2994a5e3b2701f03921d250273813b
d3 <dsmart2080@gmail.com>
2022-05-23 18:28:42 -0600

    create friendship model

---
+++ app/models/friendship.rb
@@ -0,0 +1,16 @@
+class Friendship < ApplicationRecord
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+    
+    belongs_to :friend,
+        primary_key: :id,
+        foreign_key: :friend_id,
+        class_name: :User
+
+
+        
+
+
+end
