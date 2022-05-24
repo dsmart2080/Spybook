5b8ef66919e157ab1b52a0c5152ce40bbb2d6f8c
d3 <dsmart2080@gmail.com>
2022-05-23 20:47:50 -0600

    added attaching profile picture and cover photo functions in usersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,5 +21,25 @@ class Api::UsersController < ApplicationController
         render  json: user, include: ['wall_posts', 'wall_posts.comments', 'wall_posts.comments.author', 'wall_posts.likes']
     end
 
+    def attach_new_profile_picture
+        user = User.find_by(id: params[:id])
+        if user.profile_picture.attached?
+            user.profile_picture.purge_later
+        end
+
+        user.profile_picture.attach(params[:profile_picture])
+        render json: user, include: ['wall_posts', 'wall_posts.comments', 'wall_posts.comments', 'wall_posts.likes', 'wall_posts.author','assertive_friendships']
+    end
+
+
+    def attach_new_cover_photo
+        user = User.find_by(id: params[:id])
+        if user.cover_photo.attached?
+            user.cover_photo.purge_later
+        end
+
+        user.cover_photo.attach(params[:cover_photo])
+        render json: user, include: ['wall_posts', 'wall_posts.comments','wall_posts.comments.author', 'wall_posts.likes', 'wall_posts.author', 'assertive_friendships']
+    end
 
 end
