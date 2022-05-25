d378e415292102aed1601e9cbf3d8e6963e66dfb
d3 <dsmart2080@gmail.com>
2022-05-25 02:23:10 -0600

    updated formating for comment component

--- client/components/Comment.js
+++ client/components/Comment.js
@@ -3,9 +3,9 @@ import {useState} from 'react';
 import FormToEditComment from './FormToEditComment';
 import blankProfilePicture from '../images/blank_profile_picture.png';
 
-function Comment({ postsComment, user, setPostsCommentsWrapperToRemoveComment, setPostsCommentsWrapperToUpdateComment}){
-    
-    
+function Comment({ postsComment, user, setPostsCommentsWrapperToRemoveComment, setPostsCommentsWrapperToUpdateComment})
+{
+    //State management
     const [isEditingComment, setIsEditingComment] = useState(false);
 
     if (isEditingComment){
@@ -18,51 +18,54 @@ function Comment({ postsComment, user, setPostsCommentsWrapperToRemoveComment, s
             />
         );
     }
+
+
+    function deleteCommentHandler(){
+        fetch('/api/comments/${postsComment.id}', {
+            method: 'DELETE'
+        })
+        .then(response => {
+            if (response.ok) {
+                response.json().then(comment => setPostsCommentsWrapperToRemoveComment(comment));
+            }
+        });
+    }
+
+    function editCommentHandler(){
+        setIsEditingComment(!isEditingComment);
+    }
+
+
+    return (
+        <article className='comment'>
+            <Link to={`/users/${postsComment.author_id}`} title={postsComment.author.first_name} className='thumb'>
+                <img 
+                src={!postsComment.author.profile_picture_url ? blankProfilePicture : postsComment.author.profile_picture}
+                alt=''
+                />
+            </Link>
+            <div className='comment-body'>
+                <h2>
+                    <Link to={`/users/${postsComment.author.id}`}>
+                        {`${postsComment.author.first_name}`}
+                    </Link>
+                </h2>
+                <p>{postsComment.body}</p>
+                <footer className='comment-footer'>
+                    <ul className='comment-footer-tools'>
+                        {postsComment.author_id === user.id ? 
+                        <>
+                        <li><button onClick={deleteCommentHandler}>Delete</button></li>
+                        <li><button onClick={editCommentHandler}>Exit</button></li>
+                        </>
+                        : null}
+                    </ul>
+                </footer>
+            </div>
+        </article>
+    );
+
 }
 
-function deleteCommentHandler(){
-    fetch('/api/comments/${postsComment.id}'), {
-        method: 'DELETE'
-    })
-    .then(response => {
-        if (response.ok) {
-            response.json().then(comment => setPostsCommentsWrapperToRemoveComment(comment));
-        }
-    });
-}
 
-function editCommentHandler(){
-    setIsEditingComment(!isEditingComment);
-}
-
-
-return (
-    <article className='comment'>
-        <Link to={`/users/${postsComment.author_id}`} title={postsComment.author.first_name} className='thumb'>
-            <img 
-            src={!postsComment.author.profile_picture_url ? blankProfilePicture : postsComment.author.profile_picture}
-            alt=''
-            />
-        </Link>
-        <div className='comment-body'>
-            <h2>
-                <Link to={`/users/${postsComment.author.id}`}>
-                    {`${postsComment.author.first_name}`}
-                </Link>
-            </h2>
-            <p>{postsComment.body}</p>
-            <footer className='comment-footer'>
-                <ul className='comment-footer-tools'>
-                    {postsComment.author_id === user.id ? 
-                    <>
-                    <li><button onClick={deleteCommentHandler}>Delete</button></li>
-                    <li><button onClick={editCommentHandler}>Exit</button></li>
-                    </>
-                    : null}
-                </ul>
-            </footer>
-        </div>
-    </article>
-);
-
-}
+export default Comment;
