25e542ff669f76dd79b78bde02e73738766f9c82
dswe2022 <dswe2022@gmail.com>
2022-05-24 22:21:12 -0600

    updated comment return output

--- client/components/Comment.js
+++ client/components/Comment.js
@@ -21,5 +21,35 @@ function Comment({ postsComment, user, setPostsCommentsWrapperToRemoveComment, s
 }
 
 function deleteCommentHandler(){
-    
+    fetch('/api/comments/${postsComment.id}'), {
+        method: 'DELETE'
+    })
+    .then(response => {
+        if (response.ok) {
+            response.json().then(comment => setPostsCommentsWrapperToRemoveComment(comment));
+        }
+    });
 }
+
+function editCommentHandler(){
+    setIsEditingComment(!isEditingComment);
+}
+
+
+return (
+    <article className='comment'>
+        <Link to={`/users/${postsComment.author_id}`} title={postsComment.author.first_name} className='thumb'>
+            <img 
+            src={!postsComment.author.profile_picture_url ? blankProfilePicture : postsComment.author.profile_picture}
+            alt=''
+            />
+        </Link>
+        <div className='comment-body'>
+            <h2>
+                <Link to={`/users/${postsComment.author.id}`}>
+                    {`${postsComment.author.first_name}`}
+                </Link>
+            </h2>
+        </div>
+    </article>
+)
