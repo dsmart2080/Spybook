2fc6e5f79e9193d13469d99cef09893f6b62fa27
d3 <dsmart2080@gmail.com>
2022-05-25 12:58:57 -0600

    added output of the search component

---
+++ client/components/SearchSpybook.js
@@ -0,0 +1,43 @@
+import {useState, useEffect} from 'react';
+import SearchResultsContainer from './SearchResultsContainer';
+
+//Populating data from using Search
+
+function SearchSpybook(){
+    const [allUsers, setAllUsers] = useState([]);
+    const [searchString, setSearchString] = useState('');
+
+    useEffect(()=> {
+        fetch('/api/users')
+        .then(response => setAllUsers(users));
+    }, []);
+
+
+    const displayUsers = allUsers.filter(
+        user => `${user.first_name} ${user.last_name}`.toLowerCase().includes(searchString.toLowerCase())
+    );
+
+
+    function changeSearchStringHandler(event){
+        setSearchString(event.target.value);
+    }
+
+
+    return (
+        <div className='search-spybook'>
+            <input
+            type='search'
+            placeholder='Search Spyb👀b'
+            value={searchString}
+            onChange={changeSearchStringHandler}
+            />
+            <SearchResultsContainer
+                displayedUsers={displayUsers}
+                searchString={searchString}
+            />
+            
+            
+            </input>
+        </div>
+    )
+}
