a66f10f8ab603bb38f5eeab2abff005089c1975b
d3 <dsmart2080@gmail.com>
2022-05-25 03:11:14 -0600

    Fixed user loggining in issue in Navbar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -2,3 +2,64 @@ import Login from './Login';
 import {Link, useHistory} from 'react-router-dom';
 import SearchLookbook from './SearchLookbook';
 
+function NavBar({user, setUser})
+{
+    const history = useHistory();
+    //Execute if no user is present.
+    if (!user){
+        return (
+            <header className='header'>
+                <nav className='header-nav-logged-out'>
+                    <h1 className='header-logo-logged-out'>
+                        <Link exact to='/'>
+                            SpyB👀k
+                        </Link>
+                    </h1>
+                    <Login setUser={setUser}/>
+                </nav>
+            </header>
+        );
+    }
+
+
+    //Execute if logging out.
+    //Delete API call after logging.
+    function logoutHandler(){
+        fetch('/api/logout', {
+            method: 'DELETE'
+        })
+        .then(response => {
+            if(response.ok){
+                setUser(null);
+                //Execute right after logout
+                history.push('/'); 
+            }
+        });
+    }
+
+
+    //Execute once User clicks login with correct credentials
+    return (
+        <header className='header'>
+            <nav className='header-nav'>
+                <h1 className='header-logo'>
+                    <Link exact to='/home_feed'>
+                        SpyB👀k
+                    </Link>
+                </h1>
+                <SearchSpybook/>
+                <ul className='header-list'>
+                <li>
+                    <Link exact to={`/users/${user.id}`}>
+                        {user.first_name}
+                    </Link>
+                </li>
+                <li>Notifications</li>
+                <li><button onClick={logoutHandler}>Logout</button></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
