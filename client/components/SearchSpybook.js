625bcbcbf747ea310cacd504906282b2717fb530
dswe2022 <dswe2022@gmail.com>
2022-05-25 13:10:38 -0600

    Fixed searchResultsContainer error

--- client/components/SearchSpybook.js
+++ client/components/SearchSpybook.js
@@ -35,9 +35,6 @@ function SearchSpybook(){
                 displayedUsers={displayUsers}
                 searchString={searchString}
             />
-            
-            
-            </input>
         </div>
     )
 }
