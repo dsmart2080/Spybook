1c3d25c6227643727d29a3e562edfaa059be1e71
dswe2022 <dswe2022@gmail.com>
2022-05-25 14:06:46 -0600

    Updated searchResult container

---
+++ client/src/components/SearchResultsContainer.js
@@ -0,0 +1,7 @@
+import {Link} from 'react-router-dom';
+import blankProfilePicture from '../images/blank_profile_picture.png';
+
+function SearchResultsContainer({displayedUsers, searchString}){
+
+    const displayedUsersArrJSX = displayedUsers.map()
+}
