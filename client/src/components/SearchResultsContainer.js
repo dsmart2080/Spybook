061876b136cdd79650579bf1505732893a85025a
dswe2022 <dswe2022@gmail.com>
2022-05-25 14:33:24 -0600

    search results container

--- client/src/components/SearchResultsContainer.js
+++ client/src/components/SearchResultsContainer.js
@@ -3,5 +3,37 @@ import blankProfilePicture from '../images/blank_profile_picture.png';
 
 function SearchResultsContainer({displayedUsers, searchString}){
 
-    const displayedUsersArrJSX = displayedUsers.map()
+    const displayedUsersArrJSX = displayedUsers.map(user =>{
+        return (
+            <li key={user.id}>
+                <Link to={`/users/${user.id}`} title={user.first_name} className="thumb search-result-thumb">
+                    <img
+                        src={!user.profile_picture_url ? blankProfilePicture : user.profile_picture_url}
+                        alt=''
+                    />
+                </Link>
+                <h2>
+                    <Link to={`/users/${user.id}`}>
+                        {`${user.first_name} ${user.last_name}`}
+                    </Link>
+                </h2>
+            </li>
+        );
+    });
+
+
+    return (
+        <>
+            {searchString === '' ?
+                null 
+                : <ul className='search-results'>'
+                    {displayUsersArrJSX}
+                </ul>}
+        </>
+    );
 }
+
+
+export default SearchResultsContainer;
+
+
