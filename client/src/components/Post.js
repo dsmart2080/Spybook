a8182ec463939d31339dfcb4761e8bc2a5825b55
d3 <dsmart2080@gmail.com>
2022-05-26 15:12:18 -0600

    made post components

---
+++ client/src/components/Post.js
@@ -0,0 +1,74 @@
+import Comment from './Comment';
+import {Link} from 'react-router-dom';
+import FormToSubmitComment from './FormToSumitComment';
+import {useState} from 'react';
+import FormToEditPost from './FormToEditPost';
+import blankProfilePicture from '../images/blank_profile_pciture.png';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+function Post({post, user, setArbitraryUserWrapperToRemoveWallPost, setFriendsAuthroedPostsWrapper}){
+    
+    const [postsComments, setPostsComments] = useState(post.comments);
+    const [isEditingPost, setIsEdidtingPost] = useState(false);
+
+    const [postsLikes, setPostsLikes] = useState(post.likes);
+    const isPostLiked = postsLikes.map(like => like.liker_id).includes(user.id);
+
+
+    function setPostsCommentsWrapperToRemoveComment(deletedComment){
+        setPostsComments(postsComments.filter(
+            comment => comment.id !== deletedComment.id
+        ));
+    }
+
+    function setPostsCommentsWrapperToUpdateComment(updatedComment){
+        setPostsComments(postsComments.map(comment => {
+            if(comment.id === updatedComment.id){
+                return updatedComment;
+            }
+            else{
+                return comment;
+            }
+        }));
+    }
+
+    const postsCommentsArrJSX = postsComments.map(
+        postsComment => {
+            return (
+                <Comment
+                    key={postsComment.id}
+                    postsComment={postsComment}
+                    user={user}
+                    setPostsCommentsWrapperToRemoveComment={setPostsCommentsWrapperToRemoveComment}
+                    setPostsCommentsWrapperToUpdateComment={setPostsCommentsWrapperToUpdateComment}
+                />
+            );
+        }
+    );
+
+
+
+    if (isEditingPost){
+        return (
+            <FormToEditPost 
+                post={post}
+                setArbitraryUserWrapperToUpdateWallPost={setArbitraryUserWrapperToUpdateWallPost}
+                setIsEditingPost={setIsEditingPost}
+                setFriendsAuthoredPostsWrapperToUpdateAuthoredPost = {setFriendsAuthoredPostsWrapperToUpdateAuthoredPost}
+                user = {user}
+                postsLikes = {postsLikes}
+                isPostLiked={isPostLiked}
+                toggleLIkePostHandler={toggleLikePostHandler}
+                editPostHandler={editPostHandler}
+                postsCommentsArrJSX={postsCommentsArrJSX}
+                
+                >
+
+            </FormToEditPost>
+        )
+    }
+    
+
+    
+}
