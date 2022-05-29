611a2c2183badc3d1ae8019fb08d44d27cf9a3fb
dswe2022 <dswe2022@gmail.com>
2022-05-29 15:19:01 -0600

    Added new form to submit comment features Added the React ttags

--- client/src/components/FormToSubmitComment.js
+++ client/src/components/FormToSubmitComment.js
@@ -8,5 +8,52 @@ function FormToSubmitComment({post,user,setPostsCommentsWrapperToAddNewComment})
         body: ''
     });
 
-    function change
+    function changeCommentFormDataHandler(event){
+        setCOmmentFormData({
+            ...commentFormData,
+            [event.target.name]: event.target.value
+        });
+    }
+
+
+    function submitCommentFormDataHandler(event){
+        event.preventDefault();
+
+
+        fetch('/api/comments', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(commentFormData)
+        })
+        .then(response => response.json())
+        .then(comment => setPostsCommentsWrapperToAddNewComment(comment));
+
+        commentFormData.body ='';
+
+
+    }
+
+
+
+
+    return (
+        <form onSubmit={submitCOmmentFormDataHandler} className='form-to-submit-comment'>
+            <fieldset className='form-to-submit-comment-fieldset'>
+                <div className='form-to-submit-comment-input'>
+                    <input
+                        type='text'
+                        name='body'
+                        placeholder='write a comment...'
+                        value={commentFormData.body}
+                        
+                        
+                    >
+
+            </fieldset>
+
+        </form>
+
+    );
 }
