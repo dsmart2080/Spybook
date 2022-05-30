fb6b6fcdea4eec250144ec36abb6931c2d974c99
dswe2022 <dswe2022@gmail.com>
2022-05-30 03:56:43 -0600

    added updatedIndexJS imports

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './components/App';
 import {BrowserRouter} from 'react-router-dom';
 
+
+import './styles/my_reset.css';
+import './styles/NavBar.css';
+import './styles/Login.css';
+import './styles/Signup.css';
+import './styles/layout.css';
+import './styles/Footer.css';
+import './styles/cover_photo.css';
+import './styles/sidebar.css';
+import './styles/thumbs.css';
+import './styles/FormToSubmitPost.css';
+import './styles/posts.css';
+import './styles/FormToEditPost.css';
+import './styles/comments.css';
+import './styles/FormToEditComment.css';
+import './styles/FormToSubmitComment.css';
+import './styles/HomeFeed.css';
+import './styles/SearchLookbook.css';
+import './styles/SearchResultsContainer.css';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
