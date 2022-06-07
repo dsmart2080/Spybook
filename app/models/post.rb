b23131e87f6e7be1b673face2f32ae8334c0b8ed
dswe2022 <dswe2022@gmail.com>
2022-06-06 23:28:36 -0600

    add

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,31 +9,39 @@
 #  updated_at   :datetime         not null
 #  recipient_id :integer
 #
+
+
+
+
 class Post < ApplicationRecord
+    #Validations for Post title
+    #validates :title, presence: true
+  
     belongs_to :author,
-        primary_key: :id,
-        foreign_key: :author_id,
-        class_name: :User
+      primary_key: :id,
+      foreign_key: :author_id,
+      class_name: :User
     
     has_many :comments,
-        primary_key: :id,
-        foreign_key: :post_id,
-        class_name: :Comment
+      primary_key: :id,
+      foreign_key: :post_id,
+      class_name: :Comment
     
     has_many :likes,
-        primary_key: :id,
-        foreign_key: :post_id,
-        class_name: :Like
-
+      primary_key: :id,
+      foreign_key: :post_id,
+      class_name: :Like
+    
     has_many :likers,
-        through: :likes,
-        source: :liker
-
+      through: :likes,
+      source: :liker
+  
     belongs_to :wall,
-        primary_key: :id,
-        foreign_key: :recipient_id,
-        class_name: :User
-
+      primary_key: :id,
+      foreign_key: :recipient_id,
+      class_name: :User
+    
     has_one_attached :post_photo
-
-end
+  
+  end
+  
