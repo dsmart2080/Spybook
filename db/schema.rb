bc4f6d4200aa4cb8e48d3394a93be9c4325611c1
dswe2022 <dswe2022@gmail.com>
2022-07-05 17:21:44 -0600

    fixed polymorphicAssociation for Likes model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_070426) do
+ActiveRecord::Schema.define(version: 2022_06_02_005508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2022_07_05_070426) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "likeable_type"
   end
 
   create_table "posts", force: :cascade do |t|
